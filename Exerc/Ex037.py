bad6518f7864ff80c4a03232709bfc4d4ebc2270
Heitor Machado Franco <40330101+heitorucb@users.noreply.github.com>
2022-12-22 03:04:17 -0300

    Update Ex037.py

--- Exerc/Ex037.py
+++ Exerc/Ex037.py
@@ -2,8 +2,10 @@ numero = int(input('Digite um valor: '))
 print('Qual será a base para conversão\n1-para binário\n2-para octal\n3-hexadecimal')
 opcao = int(input())
 if opcao == 1:
-    print('{:#b}'.format(numero))
+    print('{}'.format(bin(numero)[2:]))
 elif opcao == 2:
-    print('{:08b}'.format(numero))
+    print('{}'.format(oct(numero)[2:]))
+elif opcao == 3:
+    print('{}'.format(hex(numero)[2:]))
 else:
-    print('{:16b}'.format(numero))
+    print('Opção inválida. Tente novamente.')
